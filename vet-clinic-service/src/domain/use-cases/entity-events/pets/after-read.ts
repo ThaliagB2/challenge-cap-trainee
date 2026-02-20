25ae55da873fc0083b3454b4bc617fa4853a0e89
Beatriz Carvalho Sousa <beatriz.sousa@numenit.com>
2026-02-20 15:40:12 -0300

    feat: refactor AfterReadPetsUseCase to AfterReadPetUseCase with updated parameters and result types

--- vet-clinic-service/src/domain/use-cases/entity-events/pets/after-read.ts
+++ vet-clinic-service/src/domain/use-cases/entity-events/pets/after-read.ts
@@ -1,14 +1,13 @@
 import { Either } from '@sweet-monads/either';
 
 import { AbstractError } from '@/domain/errors';
-import { PetProps } from '@/domain/models/db/pet';
-import { Pets } from '@models/db/models';
+import { PetForCreateProps, PetWithAgeProps } from '@/domain/models/db/pet';
 
-export interface AfterReadPetsUseCase {
-    execute(params: AfterReadPetsUseCase.Params): AfterReadPetsUseCase.Result;
+export interface AfterReadPetUseCase {
+    execute(params: AfterReadPetUseCase.Params): Promise<AfterReadPetUseCase.Result>;
 }
 
-export namespace AfterReadPetsUseCase {
-    export type Params = Pets;
-    export type Result = Promise<Either<AbstractError, PetProps[]>>;
+export namespace AfterReadPetUseCase {
+    export type Params = Required<PetForCreateProps>[];
+    export type Result = Promise<Either<AbstractError, PetWithAgeProps[]>>;
 }
