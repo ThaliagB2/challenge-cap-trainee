f55d0a26a942a42d4bea91727445bd7c374f90fe
Beatriz Carvalho Sousa <beatriz.sousa@numenit.com>
2026-02-20 15:34:40 -0300

    feat: implement after read pets use case with age calculation

---
+++ vet-clinic-service/src/data/use-cases/entity-events/pet/after-read.ts
@@ -0,0 +1,19 @@
+import { right } from '@sweet-monads/either';
+
+import { AfterReadPetUseCase } from '@/domain/use-cases/entity-events/pets';
+import { PetModel, PetWithAgeProps, PetProps } from '@/domain/models/db/pet';
+
+export class AfterReadPetUseCaseImpl implements AfterReadPetUseCase {
+    async execute(params: AfterReadPetUseCase.Params): Promise<AfterReadPetUseCase.Result> {
+        const petsWithAge = params.map((pet) => this.addPetAge(pet));
+        return right(petsWithAge);
+    }
+
+    private addPetAge(params: PetProps): PetWithAgeProps {
+        const pet = PetModel.create(params);
+        return {
+            ...pet.toObject(),
+            age: pet.calculateAge()
+        };
+    }
+}
