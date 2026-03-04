8970c82eaf04461ccb7756618e484f0ba766c9d3
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-04 09:14:31 -0300

    feat: implement before create appointment use case with validation

--- vet-clinic-service/src/data/use-cases/entity-events/appointments/before-create.ts
+++ vet-clinic-service/src/data/use-cases/entity-events/appointments/before-create.ts
@@ -1,10 +1,11 @@
 import { left, right } from '@sweet-monads/either';
 
-import { BeforeCreateAppointmentUseCase } from '@/domain/use-cases/entity-events/appointments';
 import { petsRepository, veterinariansRepository } from '@/domain/repositories';
 import { Translator } from '@/domain/utils/translator';
 import { PetsModel } from '@/domain/models/db/pets';
 import { VeterinariansModel } from '@/domain/models/db/veterinarians';
+import { BeforeCreateAppointmentUseCase } from '@/domain/use-cases/entity-events/appointments';
+import { BadRequestError, NotFoundError } from '@/domain/errors';
 
 export class BeforeCreateAppointmentsUseCaseImpl implements BeforeCreateAppointmentUseCase {
     constructor(
@@ -18,15 +19,36 @@ export class BeforeCreateAppointmentsUseCaseImpl implements BeforeCreateAppointm
     }
 
     public async execute(params: BeforeCreateAppointmentUseCase.Params): Promise<BeforeCreateAppointmentUseCase.Result> {
-        
+        const petExist = await this.validatePet(params.pet_id);
+        if (!petExist) {
+            return left(new NotFoundError('Pet não Encontrado'));
+        }
+
+        const vetExist = await this.validateVet(params.veterinarian_id);
+        if (!vetExist) {
+            return left(new NotFoundError('Veterinario não encontrado'));
+        }
+
+        if (params.procedures.length === 0) {
+            return left(new BadRequestError('Nenhum procedimento foi encontrado'));
+        }
+
+        if (!params.status) {
+            params.status = 'SCHEDULED';
+        }
+
+        const calculateTotalCost = params.procedures.reduce((total, procedure) => total + procedure.cost, 0);
+        params.totalCost = calculateTotalCost;
+
+        return right(params);
     }
 
-    private async validatePet(PetId: string): Promise<PetsModel>{
+    private async validatePet(PetId: string): Promise<PetsModel> {
         const pet = await this.petRepository.findPetsById(PetId);
         return pet;
     }
 
-    private async validateVet(veterinarianId: string): Promise<VeterinariansModel>{
+    private async validateVet(veterinarianId: string): Promise<VeterinariansModel> {
         const vet = await this.vetRepository.findVeterinarianById(veterinarianId);
         return vet;
     }
