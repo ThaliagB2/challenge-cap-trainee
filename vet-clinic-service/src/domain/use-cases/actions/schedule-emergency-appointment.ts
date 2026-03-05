c49166014099237f696d5f803fc371628313d854
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-05 13:22:13 -0300

    feat: add type ResultPayload

--- vet-clinic-service/src/domain/use-cases/actions/schedule-emergency-appointment.ts
+++ vet-clinic-service/src/domain/use-cases/actions/schedule-emergency-appointment.ts
@@ -1,12 +1,21 @@
-import { AbstractError } from '@/domain/errors';
 import { Either } from '@sweet-monads/either';
+
+import { AbstractError } from '@/domain/errors';
 import { EmergencyAppointmentProps } from '@/domain/models/db/appointments';
+import { VeterinariansModel } from '@/domain/models/db/veterinarians';
+import { PetsModel } from '@/domain/models/db/pets';
 
 export interface scheduleEmergencyAppointmentUsecase {
     execute(params: ScheduleEmergencyAppointmentUseCase.Params): Promise<ScheduleEmergencyAppointmentUseCase.Result>;
 }
-
+export type ResultPayload = {
+    hasError: boolean;
+    errorMessage?: string;
+    appointmentId?: string;
+    vet?: VeterinariansModel;
+    pet?: PetsModel;
+};
 export namespace ScheduleEmergencyAppointmentUseCase {
     export type Params = EmergencyAppointmentProps;
-    export type Result = Either<AbstractError, void>;
+    export type Result = Either<AbstractError, void | { appointment: string }>;
 }
