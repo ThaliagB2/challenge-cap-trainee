1ae54e77e2ac9201ee352d03d84f3b8e482eadf2
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-12 17:02:12 -0300

    feat: add get-owner-expense-report model

---
+++ vet-clinic-service/src/domain/models/db/get-owner-expense-report.ts
@@ -0,0 +1,45 @@
+export type OwnerExpenseReportProps = {
+    ownerId: string;
+    ownerNameFull: string;
+    totalExpenses: number;
+    appointmentCount: number;
+    averageCost: number;
+};
+
+export class OwnerExpenseReport {
+    constructor(private props: OwnerExpenseReportProps) {}
+
+    public static create(props: OwnerExpenseReportProps): OwnerExpenseReport {
+        return new OwnerExpenseReport(props);
+    }
+
+    public get ownerId(): string {
+        return this.props.ownerId;
+    }
+
+    public get ownerNameFull(): string {
+        return this.props.ownerNameFull;
+    }
+
+    public get totalExpenses(): number {
+        return this.props.totalExpenses;
+    }
+
+    public get appointmentCount(): number {
+        return this.props.appointmentCount;
+    }
+
+    public get averageCost(): number {
+        return this.props.averageCost;
+    }
+
+    public toObject(): OwnerExpenseReportProps {
+        return {
+            ownerId: this.props.ownerId,
+            ownerNameFull: this.props.ownerNameFull,
+            totalExpenses: this.props.totalExpenses,
+            appointmentCount: this.props.appointmentCount,
+            averageCost: this.props.averageCost
+        };
+    }
+}
