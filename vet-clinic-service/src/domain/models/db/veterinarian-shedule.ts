61cefc078f9684f5cd4df2ea6c09c695731eefb6
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-12 17:07:18 -0300

    feat: add VeterinarianScheduleModel

---
+++ vet-clinic-service/src/domain/models/db/veterinarian-shedule.ts
@@ -0,0 +1,67 @@
+import { data } from '@sap/cds';
+import { appointmentsProps } from './appointments';
+import { ownersProps } from './owners';
+import { PetsProps } from './pets';
+import { ProceduresProps } from './procedures';
+
+export interface VeterinarianSheduleProps {
+    id: appointmentsProps['id'];
+    date: appointmentsProps['date'];
+    status: appointmentsProps['status'];
+    isEmergency: appointmentsProps['isEmergency'];
+    totalCost: appointmentsProps['totalCost'];
+    veterinarianId: appointmentsProps['veterinarian_id'];
+    procedure: ProceduresProps[];
+    pet: PetsProps;
+    owner: ownersProps;
+}
+
+export class VeterinarianScheduleModel {
+    constructor(private props: VeterinarianSheduleProps) {}
+
+    public static create(props: VeterinarianSheduleProps): VeterinarianScheduleModel {
+        return new VeterinarianScheduleModel(props);
+    }
+
+    public get id(): string {
+        return this.props.id;
+    }
+
+    public get date(): data {
+        return this.props.date;
+    }
+
+    public get status(): string {
+        return this.props.status;
+    }
+
+    public get isEmergency(): boolean {
+        return this.props.isEmergency;
+    }
+
+    public get totalCost(): number {
+        return this.props.totalCost;
+    }
+
+    public get veterinarianId(): string {
+        return this.props.veterinarianId;
+    }
+
+    public get procedure(): ProceduresProps[] {
+        return this.props.procedure;
+    }
+
+    public get pet(): PetsProps {
+        return this.props.pet;
+    }
+
+    public get owner(): ownersProps {
+        return this.props.owner;
+    }
+
+    public toObject(): VeterinarianSheduleProps {
+        return {
+            ...this.props
+        };
+    }
+}
