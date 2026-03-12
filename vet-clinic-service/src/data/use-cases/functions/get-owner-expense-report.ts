441496d727f4aed1bbd6f006d114d9b73d3a1c19
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-12 16:54:52 -0300

    feat: implement get owner expense report function

---
+++ vet-clinic-service/src/data/use-cases/functions/get-owner-expense-report.ts
@@ -0,0 +1,46 @@
+/* eslint-disable max-lines-per-function */
+import { NotFoundError } from '@/domain/errors';
+import { OwnerExpenseReport } from '@/domain/models/db/get-owner-expense-report';
+import { appointmentsRepository, ownersRepository } from '@/domain/repositories';
+import { getOwnerExpenseReportUsecase } from '@/domain/use-cases/functions/get-owner-expense-report';
+import { Translator } from '@/domain/utils/translator';
+import { left, right } from '@sweet-monads/either';
+
+export class getOwnerExpenseReportImpl implements getOwnerExpenseReportUsecase {
+    constructor(
+        private readonly ownerRepository: ownersRepository,
+        private readonly appointmentsRepository: appointmentsRepository,
+        private readonly translator: Translator
+    ) {}
+
+    public async execute(ownerId: string): Promise<getOwnerExpenseReportUsecase.Result> {
+        const owner = await this.ownerRepository.findOwnersById(ownerId);
+
+        if (!owner) {
+            const menssage = this.translator.translate('owner_não_encontrado');
+            return left(new NotFoundError(menssage));
+        }
+
+        const shedulings = await this.appointmentsRepository.findByOwnerIdAndStatus(ownerId, 'COMPLETED');
+
+        if (shedulings.length === 0) {
+            const message = this.translator.translate('owner_não_possui_agendamentos');
+            return left(new NotFoundError(message));
+        }
+
+        const totalExpenses = shedulings.reduce((total, appointment) => total + appointment.totalCost, 0);
+        const appointmentCount = shedulings.length;
+        const averageCost = totalExpenses / appointmentCount;
+
+        // Retornar right com os dados do relatório (ownerId, ownerName, totalExpenses, appointmentCount, averageCost)
+        const reportData = OwnerExpenseReport.create({
+            ownerId,
+            ownerNameFull: owner.fullName,
+            totalExpenses,
+            appointmentCount,
+            averageCost
+        });
+
+        return right(reportData);
+    }
+}
