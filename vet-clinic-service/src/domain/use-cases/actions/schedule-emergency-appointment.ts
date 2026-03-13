0eca2c95aa76f89058cbcde37dc0be0ac848c727
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 16:09:41 -0300

    feat: implement schedule emergency appointment action and interface adjustments

--- vet-clinic-service/src/domain/use-cases/actions/schedule-emergency-appointment.ts
+++ vet-clinic-service/src/domain/use-cases/actions/schedule-emergency-appointment.ts
@@ -1,13 +1,13 @@
 import { Either } from '@sweet-monads/either';
 
 import { AbstractError } from '@/domain/errors';
-import { AppointmentForCreateProps } from '@/domain/models/db/appointment';
+import { AppointmentForCreateProps, AppointmentProps } from '@/domain/models/db/appointment';
 
 export interface ScheduleEmergencyAppointmentUseCase {
     execute(params: ScheduleEmergencyAppointmentUseCase.Params): Promise<ScheduleEmergencyAppointmentUseCase.Result>;
 }
 
 export namespace ScheduleEmergencyAppointmentUseCase {
-    export type Params = AppointmentForCreateProps[];
-    export type Result = Promise<Either<AbstractError, string>>;
+    export type Params = AppointmentForCreateProps;
+    export type Result = Promise<Either<AbstractError, AppointmentProps>>;
 }
