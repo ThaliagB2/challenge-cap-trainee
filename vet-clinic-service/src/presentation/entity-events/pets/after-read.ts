c6cefa9bcb0b28db2401ba7696349f6c5aad2864
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 17:23:33 -0300

    feat: add AfterReadPetsController for entity event handler

---
+++ vet-clinic-service/src/presentation/entity-events/pets/after-read.ts
@@ -0,0 +1,14 @@
+import { AfterReadPetsUseCase } from '@/domain/use-cases/entity-events/pets';
+import { BaseControllerImpl, BaseControllerResponse } from '@/presentation/base/controller';
+
+export class AfterReadPetsController extends BaseControllerImpl {
+    constructor(private readonly useCase: AfterReadPetsUseCase) {
+        super();
+    }
+
+    public execute(params: AfterReadPetsUseCase.Params): BaseControllerResponse {
+        const result = this.useCase.execute(params);
+        if (result.isLeft()) return this.error(result.value.code, result.value.toErrorDetails());
+        return this.success(result.value);
+    }
+}
