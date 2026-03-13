602f8bdb59e725f6c51aed96c5bd8f44a3447032
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 16:22:31 -0300

    refactor: improve appointment repository's code interactions

--- vet-clinic-service/src/infra/db/hana/repositories/appointment.ts
+++ vet-clinic-service/src/infra/db/hana/repositories/appointment.ts
@@ -4,7 +4,7 @@ import { PetProps } from '@/domain/models/db/pet';
 import { ProcedureProps } from '@/domain/models/db/procedure';
 import { VeterinarianProps } from '@/domain/models/db/veterinarian';
 import { AppointmentRepository } from '@/domain/repositories';
-import { Appointments } from '@models/db/models';
+import { Appointment, Appointments } from '@models/db/models';
 import cds from '@sap/cds';
 
 export class AppointmentRepositoryImpl implements AppointmentRepository {
@@ -16,50 +16,45 @@ export class AppointmentRepositoryImpl implements AppointmentRepository {
 
         if (appointments.length === 0) return null;
 
-        return appointments.map((appointment) =>
-            AppointmentModel.with({
-                id: appointment.id as string,
-                date: appointment.date as unknown as Date,
-                status: appointment.status as string,
-                isEmergency: appointment.isEmergency as boolean,
-                totalCost: appointment.totalCost as number,
-                notes: appointment.notes as string,
-                procedures: appointment.procedures as unknown as ProcedureProps[],
-                owner: appointment.id as unknown as OwnerProps,
-                pet: appointment.pet as unknown as PetProps,
-                veterinarian: appointment.id as unknown as VeterinarianProps
-            })
-        );
+        return appointments.map((appointment) => this.modelAppointmentObject(appointment));
     }
 
-    public async findByIdAndDate(ids: string[], date?: Date): Promise<AppointmentModel[] | null> {
-        let appointmentsQuery: cds.ql.ConstructedQuery<any> | cds.ql.ConstructedQuery<any>[];
-        if (!date) appointmentsQuery = cds.ql.SELECT.from(this.APPOINTMENT).where({ id: { in: ids } });
-        else appointmentsQuery = cds.ql.SELECT.from(this.APPOINTMENT).where({ id: { in: ids }, date: date });
-
+    public async findByVeterinarianIdAndDate(veterinarianId: string, date: Date[]): Promise<AppointmentModel[] | null> {
+        const appointmentsQuery = cds.ql.SELECT.from(this.APPOINTMENT).where({ id: veterinarianId, date: { in: date } });
         const appointments: Appointments = await cds.run(appointmentsQuery);
 
         if (appointments.length === 0) return null;
 
-        return appointments.map((appointment) =>
-            AppointmentModel.with({
-                id: appointment.id as string,
-                date: appointment.date as unknown as Date,
-                status: appointment.status as string,
-                isEmergency: appointment.isEmergency as boolean,
-                totalCost: appointment.totalCost as number,
-                notes: appointment.notes as string,
-                procedures: appointment.procedures as unknown as ProcedureProps[],
-                owner: appointment.id as unknown as OwnerProps,
-                pet: appointment.pet as unknown as PetProps,
-                veterinarian: appointment.id as unknown as VeterinarianProps
-            })
-        );
+        return appointments.map((appointment) => this.modelAppointmentObject(appointment));
+    }
+
+    public async findByPetId(petId: string): Promise<AppointmentModel[] | null> {
+        const appointmentsQuery = cds.ql.SELECT.from(this.APPOINTMENT).where({ id: petId });
+        const appointments: Appointments = await cds.run(appointmentsQuery);
+
+        if (appointments.length === 0) return null;
+
+        return appointments.map((appointment) => this.modelAppointmentObject(appointment));
     }
 
     public async bulkCreate(appointments: AppointmentModel[]): Promise<void> {
         const appointmentsData = appointments.map((app) => app.toCreationObject());
         const query = cds.ql.INSERT.into(this.APPOINTMENT).entries(appointmentsData);
         await cds.run(query);
+    }
+
+    private modelAppointmentObject(appointment: Appointment): AppointmentModel {
+        return AppointmentModel.with({
+            id: appointment.id as string,
+            date: appointment.date as unknown as Date,
+            status: appointment.status as string,
+            isEmergency: appointment.isEmergency as boolean,
+            totalCost: appointment.totalCost as number,
+            notes: appointment.notes as string,
+            procedures: appointment.procedures as unknown as ProcedureProps[],
+            owner: appointment.id as unknown as OwnerProps,
+            pet: appointment.pet as unknown as PetProps,
+            veterinarian: appointment.id as unknown as VeterinarianProps
+        });
     }
 }
