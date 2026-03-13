719147388f047afecb20f5a74c36523f1c3a2b99
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-13 19:25:39 -0300

    refactor:  afterReadPetUsecase to  AfterReadPetUseCase

--- vet-clinic-service/src/data/use-cases/entity-events/pets/after-read.ts
+++ vet-clinic-service/src/data/use-cases/entity-events/pets/after-read.ts
@@ -1,11 +1,11 @@
 import { PetsModel } from '@/domain/models/db/pets';
-import { AfterReadPetUseCase, afterReadPetUsecase } from '@/domain/use-cases/entity-events/Pets';
+import { AfterReadPetUseCase } from '@/domain/use-cases/entity-events/pets';
 import { right } from '@sweet-monads/either';
 
 type PetDataWithAge = ReturnType<PetsModel['toObject']> & { age: number };
 // tirar duvida se pode deixar esse tipo nessa camada
 
-export class AfterReadPetsUseCaseImpl implements afterReadPetUsecase {
+export class AfterReadPetsUseCaseImpl implements AfterReadPetUseCase {
     constructor() {}
 
     public async execute(params: AfterReadPetUseCase.Params): Promise<AfterReadPetUseCase.Result> {
