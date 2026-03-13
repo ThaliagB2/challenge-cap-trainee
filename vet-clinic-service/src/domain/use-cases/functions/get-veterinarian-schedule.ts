e91b3f2f28fa09b72cc7ad6887a721c1f5aa8d02
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 16:11:19 -0300

    feat: implement get veterinarian schedule function and interface adjustments

--- vet-clinic-service/src/domain/use-cases/functions/get-veterinarian-schedule.ts
+++ vet-clinic-service/src/domain/use-cases/functions/get-veterinarian-schedule.ts
@@ -2,11 +2,13 @@ import { Either } from '@sweet-monads/either';
 
 import { AbstractError } from '@/domain/errors';
 import { FullAppointmentProps } from '@/domain/models/db/appointment';
+import { SearchVeterinarianScheduleParams } from '@/domain/models/db/veterinarian';
 
-export interface GetVeterinarianSchedule {
-    execute(): Promise<GetVeterinarianSchedule.Result>;
+export interface GetVeterinarianScheduleUseCase {
+    execute(params: GetVeterinarianScheduleUseCase.Params): Promise<GetVeterinarianScheduleUseCase.Result>;
 }
 
-export namespace GetVeterinarianSchedule {
+export namespace GetVeterinarianScheduleUseCase {
+    export type Params = SearchVeterinarianScheduleParams;
     export type Result = Either<AbstractError, FullAppointmentProps[]>;
 }
