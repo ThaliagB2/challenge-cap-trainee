57c914012013f676b155c63c114c11bbc6334dbd
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 17:23:43 -0300

    feat: add controller for emergency appointment action

---
+++ vet-clinic-service/src/presentation/actions/schedule-emergency-appointment.ts
@@ -0,0 +1,14 @@
+import { ScheduleEmergencyAppointmentUseCase } from '@/domain/use-cases/actions/schedule-emergency-appointment';
+import { BaseControllerImpl, BaseControllerResponse } from '@/presentation/base/controller';
+
+export class ScheduleEmergencyAppointmentController extends BaseControllerImpl {
+    constructor(private readonly useCase: ScheduleEmergencyAppointmentUseCase) {
+        super();
+    }
+
+    public async execute(params: ScheduleEmergencyAppointmentUseCase.Params): Promise<BaseControllerResponse> {
+        const result = await this.useCase.execute(params);
+        if (result.isLeft()) return this.error(result.value.code, result.value.toErrorDetails());
+        return this.success(result.value);
+    }
+}
