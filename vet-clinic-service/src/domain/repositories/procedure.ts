c462ee8096785c04736c2c28297daf69fb6669e9
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 16:26:29 -0300

    feat: add findAll and findByIds interface methods

--- vet-clinic-service/src/domain/repositories/procedure.ts
+++ vet-clinic-service/src/domain/repositories/procedure.ts
@@ -2,4 +2,6 @@ import { ProcedureModel } from '@/domain/models/db/procedure';
 
 export interface ProcedureRepository {
     bulkCreate(procedures: ProcedureModel[]): Promise<void>;
+    findAll(): Promise<ProcedureModel[]>;
+    findByIds(ids: string[]): Promise<ProcedureModel[]>;
 }
