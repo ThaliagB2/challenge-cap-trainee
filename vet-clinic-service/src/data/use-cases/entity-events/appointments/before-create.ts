12a8717023d334703b435ac4af14ec51c3b57575
JoaoMoraes <joao.moraes@numenit.com>
2026-03-13 16:06:38 -0300

    feat: implement before create appointment use case with validation

---
+++ vet-clinic-service/src/data/use-cases/entity-events/appointments/before-create.ts
@@ -0,0 +1,151 @@
+/* eslint-disable max-lines-per-function */
+import { BadRequestError } from '@/domain/errors';
+import { AppointmentModel } from '@/domain/models/db/appointment';
+import { OwnerModel } from '@/domain/models/db/owner';
+import { PetModel } from '@/domain/models/db/pet';
+import { ProcedureModel } from '@/domain/models/db/procedure';
+import { VeterinarianModel } from '@/domain/models/db/veterinarian';
+import { OwnerRepository, PetRepository, ProcedureRepository, VeterinarianRepository } from '@/domain/repositories';
+import { BeforeCreateAppointmentUseCase } from '@/domain/use-cases/entity-events/appointments';
+import { Translator } from '@/domain/utils/translator';
+import { ValidationResult } from '@/domain/validators/common/validation-result';
+import { left, right } from '@sweet-monads/either';
+
+export class BeforeCreateAppointmentUseCaseImpl implements BeforeCreateAppointmentUseCase {
+    constructor(
+        private readonly translator: Translator,
+        private readonly ownerRepository: OwnerRepository,
+        private readonly veterinarianRepository: VeterinarianRepository,
+        private readonly petRepository: PetRepository,
+        private readonly procedureRepository: ProcedureRepository
+    ) {
+        this.ownerRepository = ownerRepository;
+        this.procedureRepository = procedureRepository;
+        this.translator = translator;
+    }
+
+    public async execute(params: BeforeCreateAppointmentUseCase.Params): Promise<BeforeCreateAppointmentUseCase.Result> {
+        try {
+            const appointment = AppointmentModel.create(params);
+
+            const validatedAppointment = appointment.validate();
+            if (validatedAppointment.hasError) {
+                const errorMessages = validatedAppointment.errorMessages.map((errorMessage) => this.translator.translate(errorMessage)).join('\n');
+                return left(new BadRequestError(errorMessages));
+            }
+
+            // Validação Owner
+            const checkOwnerValidity = await this.validateOwner(appointment);
+            if (checkOwnerValidity.hasError) {
+                const errorMessages = checkOwnerValidity.errorMessages.map((error) => this.translator.translate(error)).join('\n');
+                return left(new BadRequestError(errorMessages));
+            }
+
+            // Validação Pet
+            const checkPetValidity = await this.validatePet(appointment);
+            if (checkPetValidity.hasError) {
+                const errorMessages = checkPetValidity.errorMessages.map((error) => this.translator.translate(error)).join('\n');
+                return left(new BadRequestError(errorMessages));
+            }
+
+            // Validação Veterinarian
+            const checkVetValidity = await this.validateVeterinarian(appointment);
+            if (checkVetValidity.hasError) {
+                const errorMessages = checkVetValidity.errorMessages.map((error) => this.translator.translate(error)).join('\n');
+                return left(new BadRequestError(errorMessages));
+            }
+
+            // Validação das Procedures
+            const appProcedures = appointment.procedures.map((proc) => proc.id);
+            const procedures = await this.getProcedures(appProcedures);
+            if (!procedures) {
+                const message = this.translator.translate('noProceduresFound');
+                return left(new BadRequestError(message));
+            }
+            const validatedProcedures = this.validateProcedures(appProcedures, procedures);
+            if (validatedProcedures.hasError) return left(new BadRequestError(validatedProcedures.errorMessages.join('\n')));
+
+            // O retorno já calcula o custo total e injeta o status ao formatar o objeto Appointment
+            return right(appointment.toCreationObject());
+        } catch (error) {
+            const errorData = error as Error;
+            return left(new BadRequestError(errorData.stack, errorData.message));
+        }
+    }
+
+    private async getOwner(ownerId: string): Promise<OwnerModel[]> {
+        return this.ownerRepository.findById([ownerId]);
+    }
+
+    private async getVeterinarian(vetId: string): Promise<VeterinarianModel[]> {
+        return this.veterinarianRepository.findById([vetId]);
+    }
+
+    private async getPet(petId: string): Promise<PetModel[]> {
+        return this.petRepository.findById([petId]);
+    }
+
+    private async getOwnerPetId(owner: OwnerModel[]): Promise<PetModel[]> {
+        const ownerId = owner.map((o) => o.id);
+        return this.petRepository.findByOwnerId(ownerId);
+    }
+
+    private validatePetOwnerMatch([pet]: PetModel[], [ownerPet]: PetModel[]): ValidationResult {
+        if (pet.id.trim() !== ownerPet.id) {
+            const message = this.translator.translate({ text: 'petDoesNotMatchOwner', args: pet.id });
+            return { hasError: true, errorMessages: [message] };
+        }
+        return { hasError: false };
+    }
+
+    private async getProcedures(procedureIds: string[]): Promise<ProcedureModel[]> {
+        return this.procedureRepository.findByIds(procedureIds);
+    }
+
+    private validateProcedures(appProcedures: string[], procedures: ProcedureModel[]): ValidationResult {
+        const proceduresIds = procedures.map((proc) => proc.id);
+
+        const proceduresNotFound = appProcedures.filter((procId) => !proceduresIds.includes(procId));
+        if (proceduresNotFound.length > 0) {
+            const proceduresNotFoundIds = proceduresNotFound.join(', ');
+            const message = this.translator.translate({ text: 'specificProceduresNotFound', args: proceduresNotFoundIds });
+            return { hasError: true, errorMessages: [message] };
+        }
+        return { hasError: false };
+    }
+
+    private async validateOwner(appointment: AppointmentModel): Promise<ValidationResult> {
+        const appOwnerId = appointment.owner.id;
+        const owner = await this.getOwner(appOwnerId);
+        if (!owner) {
+            const message = this.translator.translate('noOwnerFound');
+            return { hasError: true, errorMessages: [message] };
+        }
+        return { hasError: false };
+    }
+
+    private async validatePet(appointment: AppointmentModel): Promise<ValidationResult> {
+        const appPetId = appointment.pet.id;
+        const pet = await this.getPet(appPetId);
+        if (!pet) {
+            const message = this.translator.translate('noPetFound');
+            return { hasError: true, errorMessages: [message] };
+        }
+
+        const ownerPetId = await this.getOwnerPetId(await this.getOwner(appointment.owner.id));
+        const validatedPet = this.validatePetOwnerMatch(pet, ownerPetId);
+        if (validatedPet.hasError) return { hasError: true, errorMessages: validatedPet.errorMessages };
+
+        return { hasError: false };
+    }
+
+    private async validateVeterinarian(appointment: AppointmentModel): Promise<ValidationResult> {
+        const appVetId = appointment.veterinarian.id;
+        const vet = await this.getVeterinarian(appVetId);
+        if (!vet) {
+            const message = this.translator.translate('noVeterinarianFound');
+            return { hasError: true, errorMessages: [message] };
+        }
+        return { hasError: false };
+    }
+}
