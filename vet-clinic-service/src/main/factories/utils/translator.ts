1e31d3ce74ec60f031528f363e1e79473a26524b
Beatriz Carvalho Sousa <beatriz.sousa@numenit.com>
2026-03-16 16:41:47 -0300

    feat: implement translator feature

--- vet-clinic-service/src/main/factories/utils/translator.ts
+++ vet-clinic-service/src/main/factories/utils/translator.ts
@@ -4,7 +4,7 @@ import { AsyncLocalStorage } from 'async_hooks';
 import { TranslatorImpl } from '@/infra/utils/translator/translator';
 
 export const makeTranslator = () => {
-    const resourceManager = new ResourceManager('../../../infra/utils/translator/i18n/i18n');
+    const resourceManager = new ResourceManager('../../../infra/utils/translator/i18n/messages');
     return new TranslatorImpl(resourceManager, new AsyncLocalStorage());
 };
 
