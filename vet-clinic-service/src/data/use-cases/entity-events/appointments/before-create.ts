3b06959248007f312a0184caf09b6cc834621e61
JoaoMoraes <joao.moraes@numenit.com>
2026-03-25 18:10:13 -0300

    refactor: adher execute method to best practices folowwing architecture rules

--- vet-clinic-service/src/data/use-cases/entity-events/appointments/before-create.ts
+++ vet-clinic-service/src/data/use-cases/entity-events/appointments/before-create.ts
@@ -1,13 +1,11 @@
 import { left, right } from '@sweet-monads/either';
 
-/* eslint-disable max-lines-per-function */
-import { BadRequestError } from '@/domain/errors';
 import { AppointmentModel } from '@/domain/models/db/appointment';
-import { ProcedureProps } from '@/domain/models/db/procedure';
-import { AppointmentRepository, PetRepository, VeterinarianRepository } from '@/domain/repositories';
 import { BeforeCreateAppointmentUseCase } from '@/domain/use-cases/entity-events/appointments';
 import { Translator } from '@/domain/utils/translator';
 import { ValidationResult } from '@/domain/validators/common/validation-result';
+import { AppointmentRepository, PetRepository, VeterinarianRepository } from '@/domain/repositories';
+import { BadRequestError, NotFoundError } from '@/domain/errors';
 
 export class BeforeCreateAppointmentUseCaseImpl implements BeforeCreateAppointmentUseCase {
     constructor(
@@ -24,53 +22,45 @@ export class BeforeCreateAppointmentUseCaseImpl implements BeforeCreateAppointme
 
     public async execute(params: BeforeCreateAppointmentUseCase.Params): Promise<BeforeCreateAppointmentUseCase.Result> {
         try {
-            const pet = await this.petRepository.findById({ id: params.pet_id });
-            if (!pet) {
-                const message = this.translator.translate('noPetFound');
-                return left(new BadRequestError(message));
+            const appointmentModel = AppointmentModel.create(params);
+
+            const validatePet = await this.validatePet(params.pet_id);
+            if (validatePet.hasError) {
+                return left(new NotFoundError(validatePet.errorMessages.join('\n')));
             }
 
-            const veterinarian = await this.veterinarianRepository.findById({ id: params.veterinarian_id });
-            if (!veterinarian) {
-                const message = this.translator.translate('noVeterinarianFound');
-                return left(new BadRequestError(message));
+            const validateVet = await this.validateVeterinarian(params.veterinarian_id);
+            if (validateVet.hasError) {
+                return left(new NotFoundError(validateVet.errorMessages.join('\n')));
             }
 
-            const validatedProcedures = this.validateProcedures(params.procedures);
+            const validatedProcedures = appointmentModel.validateProcedures(params.procedures);
             if (validatedProcedures.hasError) {
                 return left(new BadRequestError(validatedProcedures.errorMessages.join('\n')));
             }
 
-            const appointment = AppointmentModel.create({
-                pet_id: pet.id,
-                veterinarian_id: veterinarian.id,
-                ...params
-            });
-
-            await this.appointmentRepository.bulkCreate({ appointments: [appointment] });
-            return right(appointment.toCreationObject());
+            await this.appointmentRepository.bulkCreate({ appointments: [appointmentModel] });
+            return right(appointmentModel.toCreationObject());
         } catch (error) {
             const errorData = error as Error;
             return left(new BadRequestError(errorData.stack, errorData.message));
         }
     }
 
-    private validateProcedures(procedures: ProcedureProps[]): ValidationResult {
-        if (!procedures || procedures.length === 0) {
-            return { hasError: true, errorMessages: ['proceduresAreRequired'] };
-        }
-
+    private async validatePet(petId: string): Promise<ValidationResult> {
         const errors = [];
-        for (const proc of procedures) {
-            if (!proc.id || proc.id.trim() === '') {
-                errors.push('procedureIdIsRequired');
-            }
-            if (!proc.description || proc.description.trim() === '') {
-                errors.push('procedureDescriptionIsRequired');
-            }
-            if (!proc.cost || proc.cost <= 0) {
-                errors.push('validProcedureCostIsRequired');
-            }
+        const pet = await this.petRepository.findById({ id: petId });
+        if (!pet) {
+            errors.push(this.translator.translate('noPetFound'));
+        }
+        return { hasError: errors.length > 0, errorMessages: errors };
+    }
+
+    private async validateVeterinarian(vetId: string): Promise<ValidationResult> {
+        const errors = [];
+        const vet = await this.veterinarianRepository.findById({ id: vetId });
+        if (!vet) {
+            errors.push(this.translator.translate('noVeterinarianFound'));
         }
         return { hasError: errors.length > 0, errorMessages: errors };
     }
