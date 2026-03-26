fdde4aa07cf30e082ecf97e2497a38492eaeb4bb
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-26 17:55:32 -0300

    feat: add translator in makeBeforeCreateAppointmentUseCase

--- vet-clinic-service/src/main/factories/use-cases/entity-events/appointments/before-create.ts
+++ vet-clinic-service/src/main/factories/use-cases/entity-events/appointments/before-create.ts
@@ -1,8 +1,10 @@
 import { BeforeCreateAppointmentsUseCaseImpl } from '@/data/use-cases/entity-events/appointments/before-create';
 import { PetsRepositoryImpl, VeterinariansRepositoryImpl } from '@/infra/db/hana/repositories';
+import { makeTranslator } from '@/main/factories/utils/translator';
 
 export const makeBeforeCreateAppointmentUseCase = () => {
     const petRepository = new PetsRepositoryImpl();
     const vetRepository = new VeterinariansRepositoryImpl();
-    return new BeforeCreateAppointmentsUseCaseImpl(petRepository, vetRepository);
+    const translator = makeTranslator();
+    return new BeforeCreateAppointmentsUseCaseImpl(petRepository, vetRepository, translator);
 };
