aa6c072a3eff1ef1c056bda29b3dce8c0382f593
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-06 17:49:05 -0300

    refactor: add null to PetRepository  types of result

--- vet-clinic-service/src/domain/repositories/pet.ts
+++ vet-clinic-service/src/domain/repositories/pet.ts
@@ -1,14 +1,13 @@
 import { PetModel } from "../models/db/pet"
 
 export interface PetRepository {
-    findById(id: PetRepository.FindByIdParams): Promise <PetRepository.FindByIdResult | null>
-    findByOwnerId(onwerId: PetRepository.FindByOwnerIdParams): Promise <PetRepository.FindByOwnerIdResult | null>
+    findById(id: PetRepository.FindByIdParams): Promise <PetRepository.FindByIdResult>
+    findByOwnerId(onwerId: PetRepository.FindByOwnerIdParams): Promise <PetRepository.FindByOwnerIdResult>
 }
 
 export namespace PetRepository {
     export type FindByIdParams = string
-    export type FindByIdResult = PetModel
-
+    export type FindByIdResult = PetModel | null
     export type FindByOwnerIdParams = string
-    export type FindByOwnerIdResult = PetModel[]
+    export type FindByOwnerIdResult = PetModel[] | null
 }
