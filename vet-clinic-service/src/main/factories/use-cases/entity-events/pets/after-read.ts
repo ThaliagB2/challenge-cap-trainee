5b4c60de1701089f97dd1757b455738360f4c968
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-06 16:27:16 -0300

    feat: implement after read pets use case with age calculation

---
+++ vet-clinic-service/src/main/factories/use-cases/entity-events/pets/after-read.ts
@@ -0,0 +1,7 @@
+import { AfterReadPetsUseCaseImpl } from "@/data/use-cases/entity-events/pets/after-read";
+
+export const makeAfterReadPetsUseCase = (): AfterReadPetsUseCaseImpl => {
+    return new AfterReadPetsUseCaseImpl();
+}
+
+export const afterReadPetsUseCase = makeAfterReadPetsUseCase();
