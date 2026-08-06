473ac08441358509a29e36081625d78075a7c244
Danilo Dantas <danilo_gohan@hotmail.com>
2026-08-06 17:44:17 -0300

    refactor: add typing namespace to procedure repository

--- vet-clinic-service/src/domain/repositories/procedure.ts
+++ vet-clinic-service/src/domain/repositories/procedure.ts
@@ -1,5 +1,10 @@
 import { ProcedureModel } from '@/domain/models/db/procedure';
 
 export interface ProcedureRepository {
-    create(procedure: ProcedureModel): Promise<void>;
+    create(procedure: ProcedureRepository.CreateParams): Promise<ProcedureRepository.CreateResult>;
+}
+
+export namespace ProcedureRepository {
+    export type CreateParams = ProcedureModel;
+    export type CreateResult = void;
 }
