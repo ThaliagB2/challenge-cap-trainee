3e83551d5c8a0dc3881e18e067e7c38786b7cc2f
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-09 21:33:36 -0300

    feat: register schedule emergency appointment handler in routes

---
+++ vet-clinic-service/src/presentation/actions/schedule-emergency-appointment.ts
@@ -0,0 +1,17 @@
+import { EmergencyAppointmentParams } from "@/domain/models/db/appointment";
+import { ScheduleEmergencyAppointmentUseCase } from "@/domain/use-cases/actions/schedule-emergency-appointment";
+import { BaseControllerImpl, BaseControllerResponse } from "@/presentation/base/controller";
+
+export class ScheduleEmergencyAppointmentController extends BaseControllerImpl {
+    constructor( private readonly useCase: ScheduleEmergencyAppointmentUseCase){
+        super();
+    }
+
+    public async execute(params: EmergencyAppointmentParams): Promise<BaseControllerResponse> {
+        const result = await this.useCase.execute(params);
+        if (result.isLeft()) {
+            return this.error(result.value.code, result.value.toErrorDetails());
+        }
+        return this.success(result.value);
+    }
+}
