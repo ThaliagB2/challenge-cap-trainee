3f2478514b43a10a059c16aad3d06774e6fc126e
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-09 21:30:46 -0300

    feat: implement schedule emergency appointment use case with validation

---
+++ vet-clinic-service/src/domain/use-cases/actions/schedule-emergency-appointment.ts
@@ -0,0 +1,13 @@
+import { Either } from "@sweet-monads/either";
+
+import { AbstractError } from "@/domain/errors";
+import { AppointmentProps, EmergencyAppointmentParams } from "@/domain/models/db/appointment";
+
+export interface ScheduleEmergencyAppointmentUseCase {
+    execute(params: ScheduleEmergencyAppointmentUseCase.ScheduleEmergencyAppointmentUseCaseParams): Promise<ScheduleEmergencyAppointmentUseCase.ScheduleEmergencyAppointmentUseCaseResult>
+}
+
+export namespace ScheduleEmergencyAppointmentUseCase {
+    export type ScheduleEmergencyAppointmentUseCaseParams = EmergencyAppointmentParams;
+    export type ScheduleEmergencyAppointmentUseCaseResult = Either<AbstractError, AppointmentProps>
+}
