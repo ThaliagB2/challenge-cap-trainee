6c7b1e36f3b2de4ecdfc015cb979a029128b29f8
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-10 16:42:31 -0300

    chore: add some barrel export and change export path into path with '@'

--- vet-clinic-service/src/main/factories/controllers/actions/schedule-emergency-appointment.ts
+++ vet-clinic-service/src/main/factories/controllers/actions/schedule-emergency-appointment.ts
@@ -1,5 +1,5 @@
 import { scheduleEmergencyAppointmentUseCase } from "@/main/factories/use-cases/actions";
-import { ScheduleEmergencyAppointmentController } from "@/presentation/actions/schedule-emergency-appointment";
+import { ScheduleEmergencyAppointmentController } from "@/presentation/actions";
 
 const makeScheduleEmergencyAppointmentController = (): ScheduleEmergencyAppointmentController => {
     return new ScheduleEmergencyAppointmentController(scheduleEmergencyAppointmentUseCase);
