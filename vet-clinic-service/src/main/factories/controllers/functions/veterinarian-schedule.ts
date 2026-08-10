09af43c9d6d42d84703f8e467f64a62f86e14d7b
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-10 15:44:55 -0300

    feat: add get veterinarian schedule controller

---
+++ vet-clinic-service/src/main/factories/controllers/functions/veterinarian-schedule.ts
@@ -0,0 +1,8 @@
+import { getVeterinarianScheduleItemUseCase } from "@/main/factories/use-cases/functions/veterinarian-schedule"
+import { GetVeterinarianScheduleItemController } from "@/presentation/functions"
+
+const makeGetVeterinarianScheduleItemController = (): GetVeterinarianScheduleItemController => {
+    return new GetVeterinarianScheduleItemController(getVeterinarianScheduleItemUseCase)
+}
+
+export const getVeterinarianScheduleItemController = makeGetVeterinarianScheduleItemController()
