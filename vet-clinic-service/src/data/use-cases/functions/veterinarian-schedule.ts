108ab899275f2be97773ba86daa15d1543488723
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-11 11:04:04 -0300

    refactor: extract  getVeterinarianScheduleItem and getParameters to a private mathod

--- vet-clinic-service/src/data/use-cases/functions/veterinarian-schedule.ts
+++ vet-clinic-service/src/data/use-cases/functions/veterinarian-schedule.ts
@@ -1,54 +1,64 @@
-import { left, right } from "@sweet-monads/either";
+import { left, right } from '@sweet-monads/either';
 
-import { NotFoundError, ServerError } from "@/domain/errors";
-import { AppointmentModel } from "@/domain/models/db/appointment";
-import { OwnerRepository, PetRepository, VeterinarianRepository } from "@/domain/repositories";
-import { AppointmentRepository } from "@/domain/repositories/appointments";
-import { GetVeterinarianScheduleItemUseCase } from "@/domain/use-cases/functions";
+import { NotFoundError, ServerError } from '@/domain/errors';
+import { AppointmentModel, ScheduleVeterinarianAppointmentProps } from '@/domain/models/db/appointment';
+import { OwnerRepository, PetRepository, VeterinarianRepository } from '@/domain/repositories';
+import { AppointmentRepository } from '@/domain/repositories/appointments';
+import { GetVeterinarianScheduleItemUseCase } from '@/domain/use-cases/functions';
 
 export class GetVeterinarianScheduleItemUseCaseImpl implements GetVeterinarianScheduleItemUseCase {
     constructor(
         private readonly veterinarianRepository: VeterinarianRepository,
         private readonly appointmentRepository: AppointmentRepository,
         private readonly petRepository: PetRepository,
-        private readonly ownerRepository: OwnerRepository,
-    ){}
+        private readonly ownerRepository: OwnerRepository
+    ) {}
 
-    public async execute(params: GetVeterinarianScheduleItemUseCase.GetVeterinarianScheduleItemUseCaseParams): Promise<GetVeterinarianScheduleItemUseCase.GetVeterinarianScheduleItemUseCaseResult> {
-        try{
-
-            const veterianarian = await this.veterinarianRepository.findById(params.veterinarian_id)
-            if(!veterianarian){
-                return left(new NotFoundError('Veterinarian not exist'))
+    public async execute(
+        params: GetVeterinarianScheduleItemUseCase.GetVeterinarianScheduleItemUseCaseParams
+    ): Promise<GetVeterinarianScheduleItemUseCase.GetVeterinarianScheduleItemUseCaseResult> {
+        try {
+            const veterianarian = await this.veterinarianRepository.findById(params.veterinarian_id);
+            if (!veterianarian) {
+                return left(new NotFoundError('Veterinarian not exist'));
             }
 
-            const start = new Date();
-            const end = new Date(Date.now() + params.days * 1000*60*60*24);
+            const parameters = this.getParameters(params);
 
-            const parameters :AppointmentRepository.FindByVeterinarianAndPeriodParams = {
-                veterinarian_id: veterianarian.id,
-                start: start,
-                end: end,
-            }
+            const appointments = await this.appointmentRepository.findByVeterinarianAndPeriod(parameters);
 
-            const appointments = await this.appointmentRepository.findByVeterinarianAndPeriod(parameters)
-            
-            if(!appointments||appointments.length == 0){
+            if (!appointments || appointments.length == 0) {
                 return left(new NotFoundError('No appointments found in this period'));
             }
 
-            const VeterinarianScheduleItem = await Promise.all(appointments.map( async (appointment: AppointmentModel) => {
-                const pet = await this.petRepository.findById(appointment.pet_id)
-                const owner = await this.ownerRepository.findById(pet.owner_id)
-                return {
-                        ...appointment.toObject(), 
-                        pet: pet?.toObject(),
-                        owner: owner?.toObject()}
-            }))
-            return right(VeterinarianScheduleItem)
-        }catch(error){
+            const VeterinarianScheduleItem = await this.getVeterinarianScheduleItem(appointments);
+            return right(VeterinarianScheduleItem);
+        } catch (error) {
             const errorData = error as Error;
             return left(new ServerError(errorData.stack, errorData.message));
         }
+    }
+    private async getVeterinarianScheduleItem(appointments: AppointmentModel[]): Promise<ScheduleVeterinarianAppointmentProps[]> {
+        return await Promise.all(
+            appointments.map(async (appointment: AppointmentModel) => {
+                const pet = await this.petRepository.findById(appointment.pet_id);
+                const owner = await this.ownerRepository.findById(pet.owner_id);
+                return {
+                    ...appointment.toObject(),
+                    pet: pet?.toObject(),
+                    owner: owner?.toObject()
+                };
+            })
+        );
+    }
+    private getParameters(params: GetVeterinarianScheduleItemUseCase.GetVeterinarianScheduleItemUseCaseParams): AppointmentRepository.FindByVeterinarianAndPeriodParams {
+        const start = new Date();
+        const end = new Date(Date.now() + params.days * 1000 * 60 * 60 * 24);
+        const parameters: AppointmentRepository.FindByVeterinarianAndPeriodParams = {
+            veterinarian_id: params.veterinarian_id,
+            start: start,
+            end: end
+        };
+        return parameters;
     }
 }
