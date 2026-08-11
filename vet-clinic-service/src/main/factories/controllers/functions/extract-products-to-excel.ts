eda33f0f4bac9d441929a06b9c729555098fd4ec
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-11 11:09:16 -0300

    chore: remove products and purchase order implementation

--- vet-clinic-service/src/main/factories/controllers/functions/extract-products-to-excel.ts
+++
@@ -1,9 +0,0 @@
-import { makeExtractProductsToExcelUseCase } from '@/main/factories/use-cases/functions/extract-products-to-excel';
-import { ExtractProductsToExcelController } from '@/presentation/functions/extract-products-to-excel';
-
-export const makeExtractProductsToExcelController = () => {
-    const useCase = makeExtractProductsToExcelUseCase();
-    return new ExtractProductsToExcelController(useCase);
-};
-
-export const extractProductsToExcelController = makeExtractProductsToExcelController();
--- vet-clinic-service/src/main/routes/index.ts
+++ vet-clinic-service/src/main/routes/index.ts
@@ -3,17 +3,12 @@ import '../config/module-alias';
 
 import { Service } from '@sap/cds';
 
-import { Pets, Products } from '@models/db/models';
-
-import { translator } from '@/main/factories/utils/translator';
-
-import { bulkCreatePurchaseOrdersController } from '@/main/factories/controllers/actions/bulk-create-purchase-orders';
 import { scheduleEmergencyAppointmentController } from '@/main/factories/controllers/actions/schedule-emergency-appointment';
 import { beforeCreateAppointmentController } from '@/main/factories/controllers/entity-events/appointments';
 import { afterReadPetsController } from '@/main/factories/controllers/entity-events/pets/after-read';
-import { afterReadProductsController } from '@/main/factories/controllers/entity-events/products/after-read';
 import { getVeterinarianScheduleItemController } from '@/main/factories/controllers/functions';
-import { extractProductsToExcelController } from '@/main/factories/controllers/functions/extract-products-to-excel';
+import { translator } from '@/main/factories/utils/translator';
+import { Pets } from '@models/db/models';
 
 export default (service: Service) => {
     service.before('*', async (request: any) => {
@@ -31,58 +26,19 @@ export default (service: Service) => {
         });
     });
 
-    service.after('READ', 'Products', (products: Products, request: any) => {
-        return translator.withLanguage(request._language, () => {
-            const result = afterReadProductsController.execute(products);
-            if (result.status >= 400) {
-                return request.reject(result.errorData);
-            }
-            request.results = result.data as Products;
-        });
-    });
-
-    service.after('READ', 'Pets', (pets: Pets, request:any) => {
+    service.after('READ', 'Pets', (pets: Pets, request: any) => {
         return translator.withLanguage(request._language, () => {
             const result = afterReadPetsController.execute(pets);
             if (result.status >= 400) {
                 return request.reject(result.errorData);
             }
             request.results = result.data as Pets;
-        })
-    })
-
-    service.on('extractProductsToExcel', async (request: any) => {
-        return translator.withLanguage(request._language, async () => {
-            const result = await extractProductsToExcelController.execute();
-            if (result.status >= 400) {
-                return request.reject(result.errorData);
-            }
-            const excelBuffer = result.data;
-
-            const res = request._.res;
-            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-            res.setHeader('Content-Disposition', 'attachment; filename=produtos.xlsx');
-            res.setHeader('Content-Length', excelBuffer.length);
-            res.setHeader('Cache-Control', 'max-age=0');
-
-            res.end(excelBuffer);
-            return;
         });
     });
 
     service.on('getVeterinarianScheduleItem', async (request: any) => {
         return translator.withLanguage(request._language, async () => {
-            const result = await getVeterinarianScheduleItemController.execute({veterinarian_id: request.data.veterinarian_id, days: request.data.days || 7});
-            if (result.status >= 400) {
-                return request.reject(result.errorData);
-            }
-            return result.data;
-        });
-    })
-
-    service.on('bulkCreatePurchaseOrders', async (request: any) => {
-        return translator.withLanguage(request._language, async () => {
-            const result = await bulkCreatePurchaseOrdersController.execute(request.data.payload);
+            const result = await getVeterinarianScheduleItemController.execute({ veterinarian_id: request.data.veterinarian_id, days: request.data.days || 7 });
             if (result.status >= 400) {
                 return request.reject(result.errorData);
             }
@@ -98,5 +54,5 @@ export default (service: Service) => {
             }
             return result.data;
         });
-    })
+    });
 };
