4d43afd7d129f1ed7cc775f86afba6f115fd92b3
Gabriel-Prudencio <gabriel.pereira@numenit.com>
2026-08-11 10:20:32 -0300

    feat: implement get owner expense report use case with calculations

---
+++ vet-clinic-service/src/main/factories/use-cases/functions/owner-expense-report.ts
@@ -0,0 +1,12 @@
+import { GetOwnerExpenseReportUseCaseImpl } from "@/data/use-cases/functions"
+import { GetOwnerExpenseReportUseCase } from "@/domain/use-cases/functions"
+import { AppointmentRepositoryImpl, OwnerRepositoryImpl, PetRepositoryImpl } from "@/infra/db/hana/repositories"
+
+const makeGetOwnerExpenseReportUseCase = (): GetOwnerExpenseReportUseCase => {
+    const ownerRepository = new OwnerRepositoryImpl;
+    const petRepository = new PetRepositoryImpl;
+    const appointmentRepository = new AppointmentRepositoryImpl;
+    return new GetOwnerExpenseReportUseCaseImpl(ownerRepository, petRepository, appointmentRepository);
+}
+
+export const getOwnerExpenseReportUseCase = makeGetOwnerExpenseReportUseCase();
