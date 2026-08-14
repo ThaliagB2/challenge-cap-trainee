6a0caae6a3fdfb443790f35654c162ed75744e2a
Danilo Dantas <danilo_gohan@hotmail.com>
2026-08-14 14:22:45 -0300

    feat: add get owner expense report controller factory

---
+++ vet-clinic-service/src/main/factories/controllers/functions/get-owner-expense-report.ts
@@ -0,0 +1,10 @@
+import { makeGetOwnerExpenseReportUseCase } from '@/main/factories/use-cases/functions/get-owner-expense-report';
+import { GetOwnerExpenseReportController } from '@/presentation/functions/get-owner-expense-report';
+
+export const makeGetOwnerExpenseReportController = () => {
+    const useCase = makeGetOwnerExpenseReportUseCase();
+
+    return new GetOwnerExpenseReportController(useCase);
+};
+
+export const getOwnerExpenseReportController = makeGetOwnerExpenseReportController();
