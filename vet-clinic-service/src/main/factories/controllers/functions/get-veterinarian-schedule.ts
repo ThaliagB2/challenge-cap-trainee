3bc55dfc7986294c3c5ff3bf5e5270ac8db6975e
Danilo Dantas <danilo_gohan@hotmail.com>
2026-08-14 13:56:04 -0300

    feat: add get veterinarian schedule controller factory

---
+++ vet-clinic-service/src/main/factories/controllers/functions/get-veterinarian-schedule.ts
@@ -0,0 +1,10 @@
+import { makeGetVeterinarianScheduleUseCase } from '@/main/factories/use-cases/functions/get-veterinarian-schedule';
+import { GetVeterinarianScheduleController } from '@/presentation/functions/get-veterinarian-schedule';
+
+export const makeGetVeterinarianScheduleController = () => {
+    const useCase = makeGetVeterinarianScheduleUseCase();
+
+    return new GetVeterinarianScheduleController(useCase);
+};
+
+export const getVeterinarianScheduleController = makeGetVeterinarianScheduleController();
